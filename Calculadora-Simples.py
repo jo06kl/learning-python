da46b11dc3b1902143ad87751075b5f030e97670
Jhon_xPG <jhonlui6644@gmail.com>
2023-10-08 15:09:13 -0300

    initial commit

---
+++ Calculadora-Simples.py
@@ -0,0 +1,21 @@
+print("Calculadora simples:\n")
+print("lets calculate 1+2=3 2*1=2  \n")
+
+
+n1 = float(input("Digite o primeiro número:"))
+n2 = float(input("Digite o segundo  número::"))
+op = input("Digite a operaçao matemática:")
+
+
+if op == "+":
+    resto = n1 + n2
+elif op == "-":
+    resto = n1 - n2
+elif op == "*":
+    resto = n1 * n2
+elif op == "/":
+    resto = n1 / n2
+else:
+    resto = " Atenção! operaçao invalida"
+
+print(f" Resultado  da operação {n1:.2f} {op} {n2:.2f} = {resto:.2f} ")
